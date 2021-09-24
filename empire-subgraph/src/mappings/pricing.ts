fc4fd68c6fecf901ae920978ff8a5ae77daf480e
Isaac Frank <codingwithisaac@gmail.com>
2021-09-24 08:39:04 +0100

    Remove Whitelisted (ethereum/uniswap) tokens

--- empire-subgraph/src/mappings/pricing.ts
+++ empire-subgraph/src/mappings/pricing.ts
@@ -32,20 +32,21 @@ export function getBnbPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // WBNB
-  "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
-  "0x55d398326f99059ff775485246999027b3197955", // USDT
-  "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
-  "0x23396cf899ca06c4472205fc903bdb4de249d6fc", // UST
-  "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
-  "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // ETH
-  "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82", // CAKE
-  "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5", // BAKE
-  "0x190b589cf9Fb8DDEabBFeae36a813FFb2A702454", // BDO
-  "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402", // DOT
-  "0x4fa7163e153419e0e1064e418dd7a99314ed27b6", // HOT CROSS
-  "0xc9849e6fdb743d08faee3e34dd2d1bc69ea11a51", //PANCAKE BUNNY
-  "0x4b0f1812e5df2a09796481ff14017e6005508003" // TWT
+  WBNB_ADDRESS
+  // "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // WBNB
+  // "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+  // "0x55d398326f99059ff775485246999027b3197955", // USDT
+  // "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+  // "0x23396cf899ca06c4472205fc903bdb4de249d6fc", // UST
+  // "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
+  // "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // ETH
+  // "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82", // CAKE
+  // "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5", // BAKE
+  // "0x190b589cf9Fb8DDEabBFeae36a813FFb2A702454", // BDO
+  // "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402", // DOT
+  // "0x4fa7163e153419e0e1064e418dd7a99314ed27b6", // HOT CROSS
+  // "0xc9849e6fdb743d08faee3e34dd2d1bc69ea11a51", //PANCAKE BUNNY
+  // "0x4b0f1812e5df2a09796481ff14017e6005508003" // TWT
 ];
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
